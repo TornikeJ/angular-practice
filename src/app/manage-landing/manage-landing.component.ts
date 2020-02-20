d17d09d431ba33175055f635d9a5f49c40139184
Tornike Janashia <t.janashia11@gmail.com>
2020-02-21 00:01:58 +0400

    feat(manage-landing component): add functional

--- src/app/manage-landing/manage-landing.component.ts
+++ src/app/manage-landing/manage-landing.component.ts
@@ -1,11 +1,13 @@
-import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef, OnDestroy } from '@angular/core';
+import { timer } from 'rxjs/internal/observable/timer';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-manage-landing',
   templateUrl: './manage-landing.component.html',
   styleUrls: ['./manage-landing.component.scss']
 })
-export class ManageLandingComponent implements OnInit {
+export class ManageLandingComponent implements OnInit, OnDestroy {
 
   @ViewChild('feedbacks', { static: false }) review: ElementRef;
 
@@ -17,17 +19,22 @@ export class ManageLandingComponent implements OnInit {
   thirdColumn = 8 * (this.width + this.margin);
   startX;
   updatedX;
+  displayElement = ['flex', 'flex', 'flex'];
   scrollX: number[] = [this.firstColumn, this.secondColumn, this.thirdColumn];
   position: number[] = [this.firstColumn, this.secondColumn, this.thirdColumn];
   backgroundStyle = 'url(\'assets/manage-landing/bg-tablet-pattern.svg\')' + ',' + 'url(\'assets/manage-landing/bg-tablet-pattern.svg\')';
-  
+  subscription: Subscription;
+  source = timer(500, 40);
+
   constructor() { }
 
   ngOnInit() {
+    this.startTimer();
   }
 
   setEvent() {
     let mouseDown = false;
+    this.subscription.unsubscribe();
 
     this.review.nativeElement.addEventListener('mousedown', (e: MouseEvent) => {
       this.startX = e.pageX;
@@ -37,12 +44,14 @@ export class ManageLandingComponent implements OnInit {
       mouseDown = false;
       for (let i = 0; i < this.position.length; i++) {
         this.position[i] = this.scrollX[i];
+        this.displayElement[i] = 'flex';
       }
     });
     this.review.nativeElement.addEventListener('mouseleave', (e: MouseEvent) => {
       mouseDown = false;
       for (let i = 0; i < this.position.length; i++) {
         this.position[i] = this.scrollX[i];
+        this.displayElement[i] = 'flex';
       }
     });
     this.review.nativeElement.addEventListener('mousemove', (e: MouseEvent) => {
@@ -55,10 +64,12 @@ export class ManageLandingComponent implements OnInit {
         for (let i = 0; i < this.scrollX.length; i++) {
           if (this.scrollX[i] < this.scrollArea) {
             this.scrollX[i] = this.checkMaxX(this.scrollX) + this.secondColumn;
+            this.displayElement[i] = 'none';
           }
 
           if (this.scrollX[i] > 3 * -this.scrollArea) {
             this.scrollX[i] = this.checkMinX(this.scrollX) - this.secondColumn;
+            this.displayElement[i] = 'none';
           }
         }
       }
@@ -75,4 +86,32 @@ export class ManageLandingComponent implements OnInit {
     return Math.min(...tempArr);
   }
 
+  startTimer() {
+    this.subscription = this.source.subscribe(
+      () => {
+        this.scrollX[0] -= 0.5;
+        this.scrollX[1] -= 0.5;
+        this.scrollX[2] -= 0.5;
+        this.displayElement[0] = 'flex';
+        this.displayElement[1] = 'flex';
+        this.displayElement[2] = 'flex';
+
+        for (let i = 0; i < this.scrollX.length; i++) {
+
+          if (this.scrollX[i] < this.scrollArea) {
+            this.scrollX[i] = this.checkMaxX(this.scrollX) + this.secondColumn;
+            this.displayElement[i] = 'none';
+          }
+
+          if (this.scrollX[i] > 3 * -this.scrollArea) {
+            this.scrollX[i] = this.checkMinX(this.scrollX) - this.secondColumn;
+            this.displayElement[i] = 'none';
+          }
+        }
+      });
+  }
+
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
+  }
 }

e5040c692dd107ccda5ea7eb8ae9650c3f245173
TornikeJ <t.janashia11@gmail.com>
2020-02-25 11:42:58 +0400

    feat(manage-landing component): finish component

--- src/app/manage-landing/manage-landing.component.ts
+++ src/app/manage-landing/manage-landing.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit, ViewChild, ElementRef, OnDestroy } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef, OnDestroy, HostListener } from '@angular/core';
 import { timer } from 'rxjs/internal/observable/timer';
-import { Subscription } from 'rxjs';
+import { Subscription, Subject } from 'rxjs';
+import { startWith, switchMap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-manage-landing',
@@ -10,6 +11,14 @@ import { Subscription } from 'rxjs';
 export class ManageLandingComponent implements OnInit, OnDestroy {
 
   @ViewChild('feedbacks', { static: false }) review: ElementRef;
+
+
+  displayStyle;
+  showDesktop = false;
+  innerWidth;
+
+  selectedReview = 1;
+  selectMode = false;
 
   width = 512;
   margin = 30;
@@ -23,57 +32,110 @@ export class ManageLandingComponent implements OnInit, OnDestroy {
   scrollX: number[] = [this.firstColumn, this.secondColumn, this.thirdColumn];
   position: number[] = [this.firstColumn, this.secondColumn, this.thirdColumn];
   backgroundStyle = 'url(\'assets/manage-landing/bg-tablet-pattern.svg\')' + ',' + 'url(\'assets/manage-landing/bg-tablet-pattern.svg\')';
-  subscription: Subscription;
+  subscriptionDesktop: Subscription;
+  subscriptionMobile: Subscription;
+  reset = new Subject();
   source = timer(500, 40);
+
+
+  @HostListener('window:resize', ['$event'])
+  onResize(event) {
+    this.innerWidth = window.innerWidth;
+    this.checkWidth();
+  }
+
 
   constructor() { }
 
   ngOnInit() {
+    this.innerWidth = window.innerWidth;
     this.startTimer();
+    this.checkWidth();
   }
 
   setEvent() {
     let mouseDown = false;
-    this.subscription.unsubscribe();
 
-    this.review.nativeElement.addEventListener('mousedown', (e: MouseEvent) => {
-      this.startX = e.pageX;
-      mouseDown = true;
-    });
-    this.review.nativeElement.addEventListener('mouseup', (e: MouseEvent) => {
-      mouseDown = false;
-      for (let i = 0; i < this.position.length; i++) {
-        this.position[i] = this.scrollX[i];
-        this.displayElement[i] = 'flex';
-      }
-    });
-    this.review.nativeElement.addEventListener('mouseleave', (e: MouseEvent) => {
-      mouseDown = false;
-      for (let i = 0; i < this.position.length; i++) {
-        this.position[i] = this.scrollX[i];
-        this.displayElement[i] = 'flex';
-      }
-    });
-    this.review.nativeElement.addEventListener('mousemove', (e: MouseEvent) => {
-      this.updatedX = e.pageX;
-      if (mouseDown) {
-        this.scrollX[0] = this.position[0] + this.updatedX - this.startX;
-        this.scrollX[1] = this.position[1] + this.updatedX - this.startX;
-        this.scrollX[2] = this.position[2] + this.updatedX - this.startX;
+    this.subscriptionDesktop.unsubscribe();
 
-        for (let i = 0; i < this.scrollX.length; i++) {
-          if (this.scrollX[i] < this.scrollArea) {
-            this.scrollX[i] = this.checkMaxX(this.scrollX) + this.secondColumn;
-            this.displayElement[i] = 'none';
-          }
+    if (this.innerWidth > 600) {
 
-          if (this.scrollX[i] > 3 * -this.scrollArea) {
-            this.scrollX[i] = this.checkMinX(this.scrollX) - this.secondColumn;
-            this.displayElement[i] = 'none';
-          }
+      this.review.nativeElement.addEventListener('mousedown', (e: MouseEvent) => {
+        this.startX = e.pageX;
+        mouseDown = true;
+      });
+
+      this.review.nativeElement.addEventListener('touchstart', (e: TouchEvent) => {
+        this.startX = e.touches[0].clientX;
+        mouseDown = true;
+      });
+
+      this.review.nativeElement.addEventListener('mouseup', (e: MouseEvent) => {
+        mouseDown = false;
+        for (let i = 0; i < this.position.length; i++) {
+          this.position[i] = this.scrollX[i];
+          this.displayElement[i] = 'flex';
         }
+      });
+
+      this.review.nativeElement.addEventListener('touchcancel', (e: TouchEvent) => {
+        mouseDown = false;
+        for (let i = 0; i < this.position.length; i++) {
+          this.position[i] = this.scrollX[i];
+          this.displayElement[i] = 'flex';
+        }
+      });
+
+      this.review.nativeElement.addEventListener('mouseleave', (e: MouseEvent) => {
+        mouseDown = false;
+        for (let i = 0; i < this.position.length; i++) {
+          this.position[i] = this.scrollX[i];
+          this.displayElement[i] = 'flex';
+        }
+      });
+
+      this.review.nativeElement.addEventListener('touchend', (e: TouchEvent) => {
+        mouseDown = false;
+        for (let i = 0; i < this.position.length; i++) {
+          this.position[i] = this.scrollX[i];
+          this.displayElement[i] = 'flex';
+        }
+      });
+
+      this.review.nativeElement.addEventListener('mousemove', (e: MouseEvent) => {
+        this.updatedX = e.pageX;
+        if (mouseDown) {
+          this.moveElements();
+        }
+      });
+
+      this.review.nativeElement.addEventListener('touchmove', (e: TouchEvent) => {
+        this.updatedX = e.touches[0].clientX;
+
+        if (mouseDown) {
+          this.moveElements();
+        }
+      });
+    }
+  }
+
+  moveElements() {
+
+    this.scrollX[0] = this.position[0] + this.updatedX - this.startX;
+    this.scrollX[1] = this.position[1] + this.updatedX - this.startX;
+    this.scrollX[2] = this.position[2] + this.updatedX - this.startX;
+
+    for (let i = 0; i < this.scrollX.length; i++) {
+      if (this.scrollX[i] < this.scrollArea) {
+        this.scrollX[i] = this.checkMaxX(this.scrollX) + this.secondColumn;
+        this.displayElement[i] = 'none';
       }
-    });
+
+      if (this.scrollX[i] > 3 * -this.scrollArea) {
+        this.scrollX[i] = this.checkMinX(this.scrollX) - this.secondColumn;
+        this.displayElement[i] = 'none';
+      }
+    }
   }
 
   checkMaxX(position: number[]): number {
@@ -86,32 +148,115 @@ export class ManageLandingComponent implements OnInit, OnDestroy {
     return Math.min(...tempArr);
   }
 
+  menuClicked() {
+    this.showDesktop = !this.showDesktop;
+
+    if (this.showDesktop) {
+      this.displayStyle = this.showDesktop && 'flex';
+    }
+    else {
+      this.displayStyle = 'none';
+    }
+  }
+
+
+  checkWidth() {
+    if (this.innerWidth > 820) {
+      this.displayStyle = 'flex';
+    } else {
+      this.showDesktop = false;
+      this.displayStyle = 'none';
+    }
+
+    if (this.innerWidth < 600) {
+      this.displayElement[0] = 'none';
+      this.displayElement[2] = 'none';
+      this.scrollArea = 0;
+      this.scrollX[1] = 0;
+      this.selectMode = true;
+      if (this.subscriptionDesktop) {
+        this.subscriptionDesktop.unsubscribe();
+      }
+      this.startScrolling();
+    } else {
+      this.selectMode = false;
+      this.scrollArea = -4 * this.width;
+      this.scrollX = [this.firstColumn, this.secondColumn, this.thirdColumn];
+      this.position = [this.firstColumn, this.secondColumn, this.thirdColumn];
+      if (this.subscriptionMobile) {
+        this.subscriptionMobile.unsubscribe();
+      }
+      this.startTimer();
+    }
+  }
+
+
+
   startTimer() {
-    this.subscription = this.source.subscribe(
+    if (this.subscriptionDesktop) {
+      this.subscriptionDesktop.unsubscribe();
+    }
+
+    this.source = timer(500, 40);
+    this.subscriptionDesktop = this.source.subscribe(
       () => {
-        this.scrollX[0] -= 0.5;
-        this.scrollX[1] -= 0.5;
-        this.scrollX[2] -= 0.5;
-        this.displayElement[0] = 'flex';
-        this.displayElement[1] = 'flex';
-        this.displayElement[2] = 'flex';
+        if (this.innerWidth > 600) {
 
-        for (let i = 0; i < this.scrollX.length; i++) {
+          this.scrollX[0] -= 0.5;
+          this.scrollX[1] -= 0.5;
+          this.scrollX[2] -= 0.5;
+          this.displayElement[0] = 'flex';
+          this.displayElement[1] = 'flex';
+          this.displayElement[2] = 'flex';
 
-          if (this.scrollX[i] < this.scrollArea) {
-            this.scrollX[i] = this.checkMaxX(this.scrollX) + this.secondColumn;
-            this.displayElement[i] = 'none';
-          }
+          for (let i = 0; i < this.scrollX.length; i++) {
 
-          if (this.scrollX[i] > 3 * -this.scrollArea) {
-            this.scrollX[i] = this.checkMinX(this.scrollX) - this.secondColumn;
-            this.displayElement[i] = 'none';
+            if (this.scrollX[i] < this.scrollArea) {
+              this.scrollX[i] = this.checkMaxX(this.scrollX) + this.secondColumn;
+              this.displayElement[i] = 'none';
+            }
+
+            if (this.scrollX[i] > 3 * -this.scrollArea) {
+              this.scrollX[i] = this.checkMinX(this.scrollX) - this.secondColumn;
+              this.displayElement[i] = 'none';
+            }
           }
         }
       });
   }
 
+  startScrolling() {
+    if (this.subscriptionMobile) {
+      this.subscriptionMobile.unsubscribe();
+    }
+    this.subscriptionMobile = this.reset.pipe(
+      startWith(0),
+      switchMap(() => timer(5000, 5000))
+    ).subscribe(
+      () => {
+        this.selectedReview++;
+
+        if (this.selectedReview == 5) {
+          this.selectedReview = 1;
+        }
+      }
+    );
+  }
+
+  selectReview(number: number) {
+    this.selectedReview = number;
+    this.reset.next();
+  }
+
+
   ngOnDestroy() {
-    this.subscription.unsubscribe();
+    if(this.subscriptionDesktop){
+      this.subscriptionDesktop.unsubscribe();
+    }
+
+    if(this.subscriptionMobile){
+      this.subscriptionMobile.unsubscribe();
+    }
+    
   }
 }

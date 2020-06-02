2a23eebced65ea8757b137f8ac72335355b4dabc
Tornike Janashia <t.janashia11@gmail.com>
2020-06-02 11:27:48 +0400

    feat(conference component): finish styling

--- src/app/conference/conference.component.ts
+++ src/app/conference/conference.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-conference',
@@ -6,10 +6,43 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./conference.component.scss', '../../assets/conference/stylesheets/reset.scss']
 })
 export class ConferenceComponent implements OnInit {
+  displayStyle;
+  showDesktop = false;
+  innerWidth;
 
-  constructor() { }
-
-  ngOnInit() {
+  @HostListener('window:resize', ['$event'])
+  onResize(event) {
+    this.innerWidth = window.innerWidth;
+    this.checkWidth();
   }
 
+  constructor() {
+
+  }
+
+  ngOnInit() {
+    this.innerWidth = window.innerWidth;
+    this.checkWidth();
+
+  }
+
+  checkWidth() {
+    if (this.innerWidth > 510) {
+      this.displayStyle = 'block';
+    } else {
+      this.showDesktop = false;
+      this.displayStyle = 'none';
+    }
+  }
+
+  menuClicked() {
+    this.showDesktop = !this.showDesktop;
+
+    if (this.showDesktop) {
+      this.displayStyle = this.showDesktop && 'flex';
+    }
+    else {
+      this.displayStyle = 'none';
+    }
+  }
 }
